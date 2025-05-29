ddeeb8721d6b8aeb1260a08adc6d3cfbffc1ca29
Rich <richbport@yahoo.com>
2025-05-29 13:21:31 -0400

    Q1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('test')
+function showRating(rating) {
+  let ratings = "";
+  for (let i = 0; i < 4; ++i) {
+    ratings = ratings + "*";
+    if (i !== 3) {
+      ratings = ratings + " ";
+    }
+  }
+  return ratings;
+}
+
+
+console.log(showRating(4));
