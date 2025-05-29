208d3cfec57130cd64cf89c6b818ccc3218fb269
Rich <richbport@yahoo.com>
2025-05-29 18:16:12 -0400

    Q1 integer

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 function showRating(rating) {
   let ratings = "";
-  //   for (let i = 0; i < rating; ++i) {
-  //     ratings = ratings + "*";
-  //     if (i !== rating - 1) {
-  //       ratings = ratings + " ";
-  //     }
-  //   }
-  //   return ratings;
-  for (let i = 0; i < 4.5; ++i) {
-    console.log(i);
-  }
+    for (let i = 0; i < Math.floor(rating); ++i) {
+      ratings = ratings + "*";
+      if (i !== Math.floor(rating - 1)) {
+        ratings = ratings + " ";
+      }
+    }
+    return ratings;
 }
 
 console.log(showRating(4.5));
