ce6e3894dd1455462f202d169a9d4c0736d398d5
Rich <richbport@yahoo.com>
2025-05-29 14:57:17 -0400

    Q1 for loop alternate

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 function showRating(rating) {
   let ratings = "";
-  for (let i = 0; i < rating; ++i) {
-    ratings = ratings + "*";
-    if (i !== rating - 1) {
-      ratings = ratings + " ";
-    }
+  //   for (let i = 0; i < rating; ++i) {
+  //     ratings = ratings + "*";
+  //     if (i !== rating - 1) {
+  //       ratings = ratings + " ";
+  //     }
+  //   }
+  //   return ratings;
+  for (let i = 0; i < 4.5; ++i) {
+    console.log(i);
   }
-  return ratings;
 }
 
-console.log(showRating(2));
+console.log(showRating(4.5));
