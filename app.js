5e351982b7fa7f3428a67bf9df12dfc793deb7d6
Rich <richbport@yahoo.com>
2025-05-30 21:53:26 -0400

    Q1 cleaner with +=

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 function showRating(rating) {
   let ratings = "";
   for (let i = 0; i < Math.floor(rating); ++i) {
-    ratings = ratings + "*";
-    if (i !== Math.floor(rating) - 1) {
-      ratings = ratings + " ";
+    ratings += "*";
+    if (i !== Math.floor(rating - 1)) {
+      ratings += " ";
     }
+  }
+  if (!Number.isInteger(rating)) {
+    ratings += " .";
   }
   return ratings;
 }
 
-console.log(showRating(4.5));
+console.log(showRating(3.5));
