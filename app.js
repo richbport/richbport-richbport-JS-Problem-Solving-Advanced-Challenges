018b48b41a9b6cfe5191cde235192f783c94a5dd
Rich <richbport@yahoo.com>
2025-05-31 12:33:35 -0400

    Q3 price property

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 function sortHighToLow(numbers) {
-  return numbers.sort((a, b) => b - a);
+  return numbers.sort((a, b) => {
+    return b.price - a.price;
+  });
 }
 
 console.log(
   sortHighToLow([
     { id: 1, price: 50 },
     { id: 2, price: 0 },
-    { id: 3, price: 500 },
+    { id: 3, price: -500 },
   ])
 );
