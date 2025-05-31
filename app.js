27f2ee987b3f7a82fe042185ff91a2bc5f4b3e10
Rich <richbport@yahoo.com>
2025-05-31 10:50:44 -0400

    Q2 sort array of numbers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function sortLowToHigh(num) {
-
-  
-  
+function sortLowToHigh(numbers) {
+  return numbers.sort((a, b) => {
+    return a - b;
+});
 }
 
-console.log(sortLowToHigh(3.5));
+console.log(sortLowToHigh([1, 5, 0, 100000, 2000, 2]));
