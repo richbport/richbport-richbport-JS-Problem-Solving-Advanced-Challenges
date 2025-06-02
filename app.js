5e67d84caca3f01d2245aee166f48cb2fffe5aad
Rich <richbport@yahoo.com>
2025-06-02 14:44:08 -0400

    JS runthrough pt 4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let assignment = 23
+let itemPrice = '20'; // Price is stored as a string instead of a number
+let quantity = 3;
+let totalPrice = itemPrice * quantity;
 
-assignment += 2
-
-console.log(assignment)
+console.log("final price:", totalPrice); // Expected 60, but something went wrong
