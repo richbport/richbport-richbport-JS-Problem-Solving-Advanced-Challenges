7a2449d01eb30da2274648c346f51804878a0b1d
Rich <richbport@yahoo.com>
2025-06-02 16:47:46 -0400

    fetch

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-let itemPrice = '20'; // Price is stored as a string instead of a number
-let quantity = 3;
-let totalPrice = itemPrice * quantity;
-
-console.log("Final Price: " + totalPrice); // Expected 60, but something might have gone wrong
-// multiplying a string * a number
-
-console.log("Item Price:", itemPrice, "Type:", typeof itemPrice);
-console.log("Quantity:", quantity, "Type:", typeof quantity)
-console.log("Total Price:", totalPrice)
+console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
