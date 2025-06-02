41d2b81855ba5fad2427be7b979d9eaf34779d4c
Rich <richbport@yahoo.com>
2025-06-02 10:21:37 -0400

    JS runthrough

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-function sortHighToLow(numbers) {
-  return numbers.sort((a, b) => b.price - a.price);
-}
+let name = [12, 'John', true, "Hello World", 45.43];
 
-console.log(
-  sortHighToLow([
-    { id: 1, price: 50 },
-    { id: 2, price: 0 },
-    { id: 3, price: -500 },
-  ])
-);
+console.log(name.filter(elem => typeof elem === "string"))
