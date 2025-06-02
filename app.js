772027ec5ff4ffd8f562038892f49488e4d5b69a
Rich <richbport@yahoo.com>
2025-06-02 15:27:49 -0400

    JS runthrough pt 5

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ let itemPrice = '20'; // Price is stored as a string instead of a number
 let quantity = 3;
 let totalPrice = itemPrice * quantity;
 
-console.log("final price:", totalPrice); // Expected 60, but something went wrong
+console.log("Final Price: " + totalPrice); // Expected 60, but something might have gone wrong
+// multiplying a string * a number
+
+console.log("Item Price:", itemPrice, "Type:", typeof itemPrice);
+console.log("Quantity:", quantity, "Type:", typeof quantity)
+console.log("Total Price:", totalPrice)
