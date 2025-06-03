e9cb960eddebcd6e03c0130ac1f07f2e7a2085b2
Rich <richbport@yahoo.com>
2025-06-03 19:42:34 -0400

    response.json continued

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const emailRef = document.querySelector(".email");
-console.log(emailRef)
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-  response.json().then(data => {
-    console.log(data)
-  })
-})
-
+fetch("https://jsonplaceholder.typicode.com/users/1").then((response) => {
+  return response.json().then((data) => {
+    emailRef.innerHTML = data.username;
+  });
+});
