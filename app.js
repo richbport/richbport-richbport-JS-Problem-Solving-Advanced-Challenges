fba38916091ef83e14a0619aa2f3bf9953104959
Rich <richbport@yahoo.com>
2025-06-04 12:03:37 -0400

    JS object access

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// let student = {
-//   name: "James",
-//   email: "james123@gmail.com",
-//   age: 21,
-//   studentFull: true,
-//   modulesFinished: [0, 1, 2, 3],
-// };
+let student = {
+  name: "James",
+  email: "james123@gmail.com",
+  age: 21,
+  studentFull: true,
+  modulesFinished: [0, 1, 2, 3],
+};
 
-let name = "James"
-let email = "james123@gmail.com"
-let age = 21
-let studentFull = true
-let modulesFinished = [0, 1, 2, 3]
+console.log(student.name + " Harris");
+console.log(student.age + 45);
+
+// let name = "James"
+// let email = "james123@gmail.com"
+// let age = 21
+// let studentFull = true
+// let modulesFinished = [0, 1, 2, 3]
