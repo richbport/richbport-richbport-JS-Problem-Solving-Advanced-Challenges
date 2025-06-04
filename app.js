3c0aeea3fdc993fbb88fef5f8fe82e4a57c6efbc
Rich <richbport@yahoo.com>
2025-06-04 13:16:06 -0400

    JS promises continued

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-const bootcamp = {
-  name: "Frontend Simplified",
-  students: 100,
-};
+const emailRef = document.querySelector(".email")
 
-//Overwrite the 'students' property
-bootcamp.students = 150;
-console.log(bootcamp.students);
-
-//Add a new property, 'teacher'
-bootcamp.teacher = "David Bragg";
-console.log(bootcamp.teacher);
-
-// let student = {
-//   name: "James",
-//   email: "james123@gmail.com",
-//   age: 21,
-//   studentFull: true,
-//   modulesFinished: [0, 1, 2, 3],
-// };
-
-// console.log(student.name + " Harris");
-// console.log(student.age + 45);
-
-// let name = "James"
-// let email = "james123@gmail.com"
-// let age = 21
-// let studentFull = true
-// let modulesFinished = [0, 1, 2, 3]
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+  return response.json()
+}).then(data => {
+  console.log(data)
+})
