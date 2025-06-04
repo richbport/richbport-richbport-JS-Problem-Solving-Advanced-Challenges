41c857bf026d28f7fa78bce0ae8c72da24703180
Rich <richbport@yahoo.com>
2025-06-04 12:40:45 -0400

    JS key-value change additional

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-let student = {
-  name: "James",
-  email: "james123@gmail.com",
-  age: 21,
-  studentFull: true,
-  modulesFinished: [0, 1, 2, 3],
+const bootcamp = {
+  name: "Frontend Simplified",
+  students: 100,
 };
 
-console.log(student.name + " Harris");
-console.log(student.age + 45);
+//Overwrite the 'students' property
+bootcamp.students = 150;
+console.log(bootcamp.students);
+
+//Add a new property, 'teacher'
+bootcamp.teacher = "David Bragg";
+console.log(bootcamp.teacher);
+
+// let student = {
+//   name: "James",
+//   email: "james123@gmail.com",
+//   age: 21,
+//   studentFull: true,
+//   modulesFinished: [0, 1, 2, 3],
+// };
+
+// console.log(student.name + " Harris");
+// console.log(student.age + 45);
 
 // let name = "James"
 // let email = "james123@gmail.com"
