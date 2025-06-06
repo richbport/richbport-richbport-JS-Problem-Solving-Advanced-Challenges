abdebe2f2fc08e2c0f63196cf99c086f2eb2e562
Rich <richbport@yahoo.com>
2025-06-05 21:12:57 -0400

    Promises exercise

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const emailRef = document.querySelector(".email")
+const emailRef = document.querySelector(".email");
 
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-  return response.json()
-}).then(data => {
-  console.log(data)
-})
+fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
