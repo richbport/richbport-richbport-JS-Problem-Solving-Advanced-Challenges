0a000543539505139316bdb5a360a58cccba4c4c
Rich <richbport@yahoo.com>
2025-06-06 10:11:08 -0400

    async/await

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const emailRef = document.querySelector(".email");
+// const emailRef = document.querySelector(".email");
 
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email
-  });
+async function main() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+  console.log(response.json())
+}
+
+main()
+
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.username
+//   });
