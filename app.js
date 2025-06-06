bd429b1358d8ee106aff3ea84af88b52c03b61e8
Rich <richbport@yahoo.com>
2025-06-06 11:25:29 -0400

    .then vs async/await

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-// const emailRef = document.querySelector(".email");
+const emailRef = document.querySelector(".email");
 
-async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-  console.log(response.json())
-}
+fetch("https://jsonplaceholder.typicode.com/users/1")
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  emailRef.innerHTML = data.username
+});
+console.log(data)
 
-main()
+// async function main() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+//   const data = await response.json()
+//   console.log(data)
+//   emailRef.innerHTML = data.email
+// }
 
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.username
-//   });
+// main()
