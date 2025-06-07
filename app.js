b3b60cac5155753eb96cbce1685595a7ef6ac0e8
Rich <richbport@yahoo.com>
2025-06-07 17:42:51 -0400

    getVideo(status)

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
-const statusRef = document.querySelector(".status")
+// const statusRef = document.querySelector(".status");
 
-function getSubscriptionStatus() {
+// function getSubscriptionStatus() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("VIP");
+//     }, 2000);
+//   });
+// }
+
+function getVideo(subscriptionStatus) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("VIP");
-    }, 2000);
+    if (getSubscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
   });
 }
 
-// .then
-// getSubscriptionStatus().then((response) => console.log(response));
-
-// Async/Await
 async function main() {
-  const status = (await getSubscriptionStatus());
-  statusRef.innerHTML = status
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  console.log(getVideo(status))
 }
 
 main();
