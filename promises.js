b3856cc69031d2b4b33784195da80a03c7deb086
Rich <richbport@yahoo.com>
2025-06-09 17:10:00 -0400

    promises in an array

--- promises.js
+++ promises.js
@@ -29,4 +29,16 @@ function createPost(post) {
   });
 }
 
-createPost({ title: "Post Three", body: "This is post three"}).then(getPosts)
+// createPost({ title: "Post Three", body: "This is post three" })
+//   .then(getPosts)
+//   .catch((err) => console.log(err));
+
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "Goodbye")
+);
+
+Promise.all([promise1, promise2, promise3]).then((values) =>
+  console.log(values)
+);
