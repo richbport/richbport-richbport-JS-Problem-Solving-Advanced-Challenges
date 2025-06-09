6ca5f70c5f177fb65578e5280d1833e8f95ba789
Rich <richbport@yahoo.com>
2025-06-09 16:18:44 -0400

    promises version

--- promises.js
+++ promises.js
@@ -13,9 +13,20 @@ function getPosts() {
   }, 1000);
 }
 
-function createPost(post, callback) {
-  setTimeout(() => {
-    posts.push(post);
-    callback();
-  }, 2000);
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+
+      const error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject("Error: Something went wrong");
+      }
+    }, 2000);
+  });
 }
+
+createPost({ title: "Post Three", body: "This is post three"}).then(getPosts)
