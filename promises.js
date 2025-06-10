30517b05db5a239ca26b3ee87d16f2d8d8f7da3b
Rich <richbport@yahoo.com>
2025-06-10 16:08:58 -0400

    promises continued

--- promises.js
+++ promises.js
@@ -29,17 +29,36 @@ function createPost(post) {
   });
 }
 
+async function fetchUsers() {
+  const res = await fetch
+  ("https://jsonplaceholder.typicode.com/users");
+
+  const data = await res.json();
+
+  console.log(data);
+}
+
+fetchUsers();
+
 // createPost({ title: "Post Three", body: "This is post three" })
 //   .then(getPosts)
 //   .catch((err) => console.log(err));
 
-const promise1 = Promise.resolve("Hello World");
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, "Goodbye")
-);
-const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+// async function init() {
+//   await createPost({ title: "Post Three", body: "This is post three" });
 
-Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-  console.log(values)
-);
+//   getPosts();
+// }
+
+// init();
+
+// const promise1 = Promise.resolve("Hello World");
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//   setTimeout(resolve, 2000, "Goodbye")
+// );
+// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json());
+
+// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+//   console.log(values)
+// );
