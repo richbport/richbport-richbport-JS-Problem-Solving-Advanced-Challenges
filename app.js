81c65dfd41bd761dbd8f3c8774dbc9664fe368c2
Rich <richbport@yahoo.com>
2025-06-12 13:14:25 -0400

    Form validation HTML

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let form = document.getElementById("myForm");
-  let message = document.getElementById("message");
 
-  if (form && message) {
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      let name = document.getElementById("name").value;
-      message.textContent = "Hello: " + name + "!"
-    });
-  } else {
-    if(!form) {
-      console.log("Form element does not exist!")
-    }
-    if(!message) {
-      console.log("Message element does not exist!")
-    }
-  }
-});
