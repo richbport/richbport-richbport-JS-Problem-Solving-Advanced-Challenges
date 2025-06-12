44ad026ebe39ece0c61a5ce8afdebd71931f1e07
Rich <richbport@yahoo.com>
2025-06-12 11:59:25 -0400

    event listener preventDefault

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   let form = document.getElementById("myForm");
   let message = document.getElementById("message");
+
+  if (form && message) {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+    });
+  } else {
+  }
 });
