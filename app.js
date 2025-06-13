990032cbdec717d12664ce2a53497f27ccfab261
Rich <richbport@yahoo.com>
2025-06-13 18:31:25 -0400

    nameError handling

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   let nameInput = document.getElementById("name");
   let emailInput = document.getElementById("email");
 
-  if(nameInput && emailInput){
-
+  if (nameInput && emailInput) {
+    nameInput.addEventListener("input", function () {
+      if (nameInput.value === "") {
+        nameError.textContent = "Name is required";
+      } else {
+        nameError.textContent = "";
+      }
+    });
   } else {
-    if(!nameInput) {
-      console.log("Name input does not exist!")
+    if (!nameInput) {
+      console.log("Name input does not exist!");
     }
-    if(!emailInput) {
-      console.log("Email input does not exist!")
+    if (!emailInput) {
+      console.log("Email input does not exist!");
     }
   }
 });
