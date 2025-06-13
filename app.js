3e3ba65d5adaf2864a6797c5b9e30b8e22dc1c02
Rich <richbport@yahoo.com>
2025-06-13 18:14:11 -0400

    JS form input

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let nameInput = document.getElementById("name");
+  let emailInput = document.getElementById("email");
 
+  if(nameInput && emailInput){
+
+  } else {
+    if(!nameInput) {
+      console.log("Name input does not exist!")
+    }
+    if(!emailInput) {
+      console.log("Email input does not exist!")
+    }
+  }
+});
