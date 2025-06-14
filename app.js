4d8ec1a07f3ae2c838944e984969c0a6895140d9
Rich <richbport@yahoo.com>
2025-06-13 20:30:09 -0400

    Q5 Pt 1

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let nameInput = document.getElementById("name");
-  let emailInput = document.getElementById("email");
+function postsByUser(userId) {
+  fetch("https://jsonplaceholder.typicode.com/posts").then((res) => {
+    console.log(res);
+    console.log(userId);
+  });
+}
 
-  if (nameInput && emailInput) {
-    nameInput.addEventListener("input", function () {
-      if (nameInput.value === "") {
-        nameError.textContent = "Name is required";
-      } else {
-        nameError.textContent = "";
-      }
-    })
-    emailInput.addEventListener("input", function () {
-      if (emailInput.value === "") {
-        emailInput.textContent = "Email is required";
-      } else if (!emailInput.value.includes("@")) {
-      emailInput.textContent = "Please enter valid email address!";
-      }  else {
-        emailInput.textContent = "";
-      }
-    }
-  );
-  } else {
-    if (!nameInput) {
-      console.log("Name input does not exist!");
-    }
-    if (!emailInput) {
-      console.log("Email input does not exist!");
-    }
-  }
-});
+postsByUser(4);
