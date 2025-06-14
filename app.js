64817e4c24dd7cfeaf38fc8892c00d14ca0c67b5
Rich <richbport@yahoo.com>
2025-06-14 10:07:56 -0400

    Q5 async await promise

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-function postsByUser(userId) {
-  fetch("https://jsonplaceholder.typicode.com/posts").then((res) => {
-    console.log(res);
-    console.log(userId);
-  });
+async function postsByUser(userId) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(await promise.json());
 }
 
 postsByUser(4);
